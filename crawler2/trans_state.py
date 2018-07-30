9c150e6d732271b94cf9e668f1956ce5621bfcd1
wagaga <wagaga@gmail.com>
2018-07-30 13:06:45 +0800

    demo

---
+++ crawler2/trans_state.py
@@ -0,0 +1,14 @@
+def state_code_compute(x):
+	from math import cos
+	scale_km = 0.5
+	lat = x['lat']
+	lon = x['long']
+	lat_to_km = lat*110.574
+	lon_to_km = abs(lon*(111.320*cos(lat)))
+	state_code_x = str(int(lat_to_km//scale_km))
+	state_code_y = str(int(lon_to_km//scale_km))
+	if len(state_code_y) == 4:
+		state_code_y = '0'+state_code_y
+	state_code = state_code_x+state_code_y
+	return state_code
+
